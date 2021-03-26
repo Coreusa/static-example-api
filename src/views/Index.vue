2d709837a4718897ed162ae09624bf857d18479a
Coreus <706874+Coreusa@users.noreply.github.com>
2021-03-26 15:32:22 +0100

    Added Vue framework and actions workflow

---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+  <h1>Welcome to the Static Example API</h1>
+  <p>
+    Access the API endpoints to retrieve data.
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
